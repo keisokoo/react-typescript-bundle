2559116f8ec435382ee7600a1a6c44fe59775fe4
keisokoo <keisokoo@gmail.com>
2021-05-08 22:06:13 +0900

    add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 const App = () => {
   React.useEffect(() => {}, [])
-  const [test, setTest] = React.useState('hah')
   return (
     <div id="main">
       <header>Hello World</header>
